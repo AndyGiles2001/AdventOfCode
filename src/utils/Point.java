c5ba358f9be9d9c5ba9bc8a15fcc26f83c018331
Andy Giles <gilesandy2001@gmail.com>
2023-12-11 22:52:58 -0800

    Added basic Point util class

---
+++ src/utils/Point.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class Point {
+
+    protected int x;
+    protected int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public long manhattanDistance(Point other) {
+        return Math.abs(x - other.x) + Math.abs(y - other.y);
+    }
+}
