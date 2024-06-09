a89095505660ccd8a69d8d32d22948794cf0681b
Andy Giles <gilesandy2001@gmail.com>
2024-06-09 02:04:23 -0700

    Simplified 2020.14.1 massively

--- src/main/year2020/day14/Part1.java
+++ src/main/year2020/day14/Part1.java
@@ -2,10 +2,8 @@ package main.year2020.day14;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import main.utils.InputOutputUtils;
 
@@ -25,7 +23,7 @@ public class Part1 {
             .forEach(line -> {
                 Matcher maskMatcher = maskPattern.matcher(line);
                 if (maskMatcher.find()) {
-                    handleMaskUpdate(maskMatcher.group(1));
+                    bitmask = maskMatcher.group(1);
                     return;
                 }
 
@@ -41,82 +39,25 @@ public class Part1 {
                 throw new RuntimeException("Could not identify pattern for line: " + line);
             });
 
-        long result = memory
-            .values()
-            .stream()
-            .collect(Collectors.summingLong(Long::longValue));
+        long result = 0L;
+        for (long value : memory.values()) {
+            result += value;
+        }
 
         System.out.println(result);
     }
 
-    private static void handleMaskUpdate(String newMask) {
-        bitmask = newMask;
-    }
-
     private static void handleMemoryUpdate(int address, long value) {
-        String bitRepresentation = getBitStringFromLong(value, 36);
-        String maskedBitRepresentation = applyBitmask(bitRepresentation, bitmask);
-        long maskedValue = getLongFromBitString(maskedBitRepresentation);
-
-        memory.put(address, maskedValue);
-    }
-
-    private static String getBitStringFromLong(long l, int numBits) {
-        StringBuilder stringBuilder = new StringBuilder();
-
-        long powerOfTwo = exponentiate(2, numBits - 1);
-
-        while (powerOfTwo > 0) {
-            if (l >= powerOfTwo) {
-                stringBuilder.append('1');
-                l -= powerOfTwo;
-            } else {
-                stringBuilder.append('0');
-            }
-
-            powerOfTwo /= 2;
-        }
-
-        return stringBuilder.toString();
-    }
-
-    private static long getLongFromBitString(String s) {
-        long result = 0L;
-
-        long powerOfTwo = 1L;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) == '1') {
-                result += powerOfTwo;
-            }
-
-            powerOfTwo *= 2;
-        }
-
-        return result;
-    }
-
-    private static String applyBitmask(String bitRepresentation, String bitmask) {
-        StringBuilder stringBuilder = new StringBuilder();
-
         for (int i = 0; i < 36; i++) {
-            char realBit = bitRepresentation.charAt(i);
             char maskBit = bitmask.charAt(i);
-
-            if (maskBit == 'X') {
-                stringBuilder.append(realBit);
-            } else {
-                stringBuilder.append(maskBit);
+            
+            if (maskBit == '1') {
+                value |= 1L << (35 - i);
+            } else if (maskBit == '0') {
+                value &= ~(1L << (35 - i));
             }
         }
 
-        return stringBuilder.toString();
-    }
-
-    private static long exponentiate(int base, int exponent) {
-        long result = 1;
-        for (int i = 0; i < exponent; i++) {
-            result *= base;
-        }
-        return result;
+        memory.put(address, value);
     }
 }
